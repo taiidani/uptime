03cbfb33d51d09a53c2c5c1cb096a64f3cea9fd1
Ryan Nixon <r.taiidani@gmail.com>
2022-02-13 13:03:16 -0800

    Flag folder, retries for AWS

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
 	"os"
 	"os/signal"
+	"path"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
@@ -14,25 +17,63 @@ import (
 
 var excludedOpts = []string{"nomad", "consul", "vault", "containerd", "cni"}
 
+var folder string
+
 func main() {
+	flags()
+
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer cancel()
 
 	fmt.Println("Initializing S3 Client")
-	client := loadClient(ctx)
+	client, err := loadClient(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	fmt.Println("Performing backup of /opt directories")
+	fmt.Printf("Performing backup of %s subdirectories\n", folder)
 	optBackup := backup.NewOperation(client)
-	if err := optBackup.Backup(ctx, "/opt", excludedOpts); err != nil {
+	if err := optBackup.Backup(ctx, folder, excludedOpts); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func loadClient(ctx context.Context) *s3.Client {
+func flags() {
+	flag.StringVar(&folder, "folder", "", "the folder to be backed up")
+	flag.Parse()
+
+	if folder == "" {
+		log.Fatal("-folder flag is required")
+	} else if !path.IsAbs(folder) {
+		log.Fatal("-folder must be an absolute path")
+	}
+}
+
+func loadClient(ctx context.Context) (*s3.Client, error) {
 	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-west-2"))
 	if err != nil {
 		log.Fatalf("unable to load SDK config, %v", err)
 	}
 
-	return s3.NewFromConfig(cfg)
+	client := s3.NewFromConfig(cfg)
+
+	// Perform a sanity check on the credentials
+	// Make a few attempts before giving up
+	for attempts := 0; attempts < 5; attempts++ {
+		select {
+		case <-ctx.Done():
+			return nil, fmt.Errorf("operation canceled")
+		default:
+			_, err := client.ListBuckets(ctx, &s3.ListBucketsInput{})
+			if err != nil {
+				log.Printf("Could not perform ListBuckets operation: %s", err)
+				time.Sleep(time.Second * 3)
+				continue
+			}
+
+			return client, nil
+		}
+	}
+
+	return nil, fmt.Errorf("unable to validate AWS credentials")
 }
